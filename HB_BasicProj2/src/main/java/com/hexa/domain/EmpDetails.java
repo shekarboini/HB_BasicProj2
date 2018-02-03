84759d60b99d9dd0f5aa7c5f5ebb1b1f585f5693
shekar <shekar4100@gmail.com>
2018-02-03 22:32:39 +0530

    First commit on HB_BasicProj2

---
+++ HB_BasicProj2/src/main/java/com/hexa/domain/EmpDetails.java
@@ -0,0 +1,31 @@
+package com.hexa.domain;
+
+public class EmpDetails {
+	
+	private int id;
+	private String name;
+	private double salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	public EmpDetails() {
+		System.out.println("EmpDetails: 0-param constructor");
+	}
+
+}
